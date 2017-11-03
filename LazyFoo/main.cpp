6c59cbf711ef667e6969630d18fee4d142b161a0
Kelsey <kelseyblair@outlook.com>
2017-11-02 21:43:44 -0700

    Initial Commit

---
+++ LazyFoo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LazyFoo
+//
+//  Created by Kelsey Blair on 2017-11-02.
+//  Copyright © 2017 Kelsey Blair. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
